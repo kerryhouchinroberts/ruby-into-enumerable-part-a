6679ebd9d4415ca87ea4424896823da08f657ad3
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-04-09 09:52:39 -0600

    complete exercise 02

--- exercises/02_product.rb
+++ exercises/02_product.rb
@@ -1,4 +1,13 @@
 def product(numbers)
+  if numbers.empty?
+    result = nil
+  else
+    result = 1
+  end
+  numbers.each do |number|
+    result = result * number
+  end
+  result
 end
 
 # ------ code above this line ------
